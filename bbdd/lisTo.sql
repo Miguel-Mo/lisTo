3534cfe979f84b0afbfd1d7aa832988b7c092e1b
Miguel <miguel.moreno@dataleanmakers.es>
2021-06-03 22:32:24 +0200

    add bbdd

--- bbdd/lisTo.sql
+++ bbdd/lisTo.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         dataleanmakers.com.es
--- Versión del servidor:         10.3.27-MariaDB - MariaDB Server
--- SO del servidor:              Linux
--- HeidiSQL Versión:             11.2.0.6213
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.11-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.1.0.6116
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -13,56 +13,101 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para admin_casakiki
+-- Volcando estructura de base de datos para admin_listo
 CREATE DATABASE IF NOT EXISTS `admin_listo` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `admin_listo`;
 
--- Volcando estructura para tabla admin_casakiki.centros
-CREATE TABLE IF NOT EXISTS `centros` (
-  `idCentro` int(11) NOT NULL AUTO_INCREMENT,
+-- Volcando estructura para tabla admin_listo.alimento
+CREATE TABLE IF NOT EXISTS `alimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `unidad_medida` int(11) NOT NULL,
   `idUsuario` int(11) DEFAULT NULL,
-  `nombreCentro` varchar(150) DEFAULT NULL,
-  `direccionCentro` varchar(150) DEFAULT NULL,
-  `poblacionCentro` varchar(150) DEFAULT NULL,
-  `cifCentro` varchar(25) DEFAULT NULL,
-  `telefonoCentro` varchar(11) DEFAULT NULL,
-  `emailAdministracionCentro` varchar(150) DEFAULT NULL,
-  `emailPedidosCentro` varchar(150) DEFAULT NULL,
-  `fechaAltaBBDD` date DEFAULT curdate(),
-  `fechaUltimaModificacion` date DEFAULT NULL,
-  PRIMARY KEY (`idCentro`),
-  KEY `idUsuario` (`idUsuario`),
-  CONSTRAINT `FK_centros_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `FK_alimento_tipo_alimento` (`tipo`),
+  KEY `FK_alimento_unidad_medida` (`unidad_medida`),
+  KEY `FK_alimento_usuarios` (`idUsuario`),
+  CONSTRAINT `FK_alimento_tipo_alimento` FOREIGN KEY (`tipo`) REFERENCES `tipo_alimento` (`id`),
+  CONSTRAINT `FK_alimento_unidad_medida` FOREIGN KEY (`unidad_medida`) REFERENCES `unidad_medida` (`id`),
+  CONSTRAINT `FK_alimento_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.centros: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `centros` DISABLE KEYS */;
-/*!40000 ALTER TABLE `centros` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.personas
-CREATE TABLE IF NOT EXISTS `personas` (
-  `idPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `idRolPersona` int(11) DEFAULT NULL,
-  `idCentroPersona` int(11) DEFAULT NULL,
-  `nombrePersona` varchar(150) DEFAULT NULL,
-  `direccionPersona` varchar(150) DEFAULT NULL,
-  `telefonoPersona` varchar(11) DEFAULT NULL,
-  `movilPersona` varchar(11) DEFAULT NULL,
-  `emailPersona` varchar(150) DEFAULT NULL,
-  `docIdentidadPersona` varchar(11) DEFAULT NULL,
-  `numCuentaPersona` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idPersona`),
-  KEY `idRolPersona` (`idRolPersona`),
-  KEY `idCentroPersona` (`idCentroPersona`),
-  CONSTRAINT `FK__centros` FOREIGN KEY (`idCentroPersona`) REFERENCES `centros` (`idCentro`),
-  CONSTRAINT `FK__roles` FOREIGN KEY (`idRolPersona`) REFERENCES `roles` (`idRol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Volcando estructura para tabla admin_listo.alimento_defecto
+CREATE TABLE IF NOT EXISTS `alimento_defecto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `unidad_medida` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.personas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.roles
+-- Volcando estructura para tabla admin_listo.lista
+CREATE TABLE IF NOT EXISTS `lista` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alimentosJSON` longtext DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT 0,
+  `fechaCreacion` timestamp DEFAULT NULL,
+  `activo` tinyint(4) DEFAULT 1,
+  `vecesMarcadaComoActiva` int(11) DEFAULT 0,
+  `tituloLista` varchar(200) DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `FK_lista_usuarios` (`idUsuario`),
+  CONSTRAINT `FK_lista_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla admin_listo.lista_temporal
+CREATE TABLE IF NOT EXISTS `lista_temporal` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idReceta` int(11) NOT NULL DEFAULT 0,
+  `idUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__receta` (`idReceta`),
+  KEY `FK_lista_temporal_usuarios` (`idUsuario`),
+  CONSTRAINT `FK__receta` FOREIGN KEY (`idReceta`) REFERENCES `receta` (`id`),
+  CONSTRAINT `FK_lista_temporal_usuarios` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla admin_listo.receta
+CREATE TABLE IF NOT EXISTS `receta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `dificultad` int(11) DEFAULT 1,
+  `tiempoEstimado` int(11) DEFAULT 1,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_receta_tiempo_aproximado` (`tiempoEstimado`),
+  CONSTRAINT `FK_receta_tiempo_aproximado` FOREIGN KEY (`tiempoEstimado`) REFERENCES `tiempo_aproximado` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla admin_listo.receta_alimento
+CREATE TABLE IF NOT EXISTS `receta_alimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idReceta` int(11) NOT NULL DEFAULT 0,
+  `idAlimento` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL DEFAULT 0,
+  `idUnidadMedida` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `FK_receta_alimento_receta` (`idReceta`),
+  KEY `FK_receta_alimento_alimento` (`idAlimento`),
+  KEY `FK_receta_alimento_unidad_medida` (`idUnidadMedida`),
+  CONSTRAINT `FK_receta_alimento_receta` FOREIGN KEY (`idReceta`) REFERENCES `receta` (`id`),
+  CONSTRAINT `FK_receta_alimento_unidad_medida` FOREIGN KEY (`idUnidadMedida`) REFERENCES `unidad_medida` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla admin_listo.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `idRol` int(11) NOT NULL AUTO_INCREMENT,
   `nombreRol` varchar(30) NOT NULL DEFAULT 'Agente',
@@ -71,97 +116,51 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`idRol`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
--- Volcando datos para la tabla admin_casakiki.roles: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` (`idRol`, `nombreRol`, `colorRol`, `nivelAccesoRol`) VALUES
-	(1, 'Administrador', '#007bff', 1);
-INSERT INTO `roles` (`idRol`, `nombreRol`, `colorRol`, `nivelAccesoRol`) VALUES
-	(2, 'Centro', '#007bff', 1);
-INSERT INTO `roles` (`idRol`, `nombreRol`, `colorRol`, `nivelAccesoRol`) VALUES
-	(3, 'Empleado', '#007bff', 1);
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.tareas
-CREATE TABLE IF NOT EXISTS `tareas` (
-  `idTarea` int(11) NOT NULL AUTO_INCREMENT,
-  `idTipoTarea` int(11) DEFAULT NULL,
-  `nombreTarea` varchar(150) DEFAULT NULL,
-  `minutosEstimadosTarea` float DEFAULT NULL,
-  `descripcionTarea` varchar(1250) DEFAULT NULL,
-  PRIMARY KEY (`idTarea`),
-  KEY `idTipoTarea` (`idTipoTarea`),
-  CONSTRAINT `FK_tareas_tareas_tipos` FOREIGN KEY (`idTipoTarea`) REFERENCES `tareas_tipos` (`idTipoTarea`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Volcando estructura para tabla admin_listo.tiempo_aproximado
+CREATE TABLE IF NOT EXISTS `tiempo_aproximado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tiempo` varchar(50) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.tareas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.tareas_pausas
-CREATE TABLE IF NOT EXISTS `tareas_pausas` (
-  `idPausa` int(11) NOT NULL AUTO_INCREMENT,
-  `idTareaPausa` int(11) DEFAULT NULL,
-  `inicioTarea` datetime DEFAULT NULL,
-  `pausaTarea` datetime DEFAULT NULL,
-  PRIMARY KEY (`idPausa`),
-  KEY `idTareaPausa` (`idTareaPausa`),
-  CONSTRAINT `FK_tareas_pausas_tareas_persona` FOREIGN KEY (`idTareaPausa`) REFERENCES `tareas_persona` (`idTareaPersona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Volcando estructura para tabla admin_listo.tipo_alimento
+CREATE TABLE IF NOT EXISTS `tipo_alimento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.tareas_pausas: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `tareas_pausas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tareas_pausas` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.tareas_persona
-CREATE TABLE IF NOT EXISTS `tareas_persona` (
-  `idTareaPersona` int(11) NOT NULL AUTO_INCREMENT,
-  `idPersona` int(11) DEFAULT NULL,
-  `fechaTarea` datetime DEFAULT NULL,
-  `inicioTarea` datetime DEFAULT NULL,
-  `finTarea` datetime DEFAULT NULL,
-  PRIMARY KEY (`idTareaPersona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Volcando estructura para tabla admin_listo.unidad_medida
+CREATE TABLE IF NOT EXISTS `unidad_medida` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.tareas_persona: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `tareas_persona` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tareas_persona` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla admin_casakiki.tareas_tipos
-CREATE TABLE IF NOT EXISTS `tareas_tipos` (
-  `idTipoTarea` int(11) NOT NULL AUTO_INCREMENT,
-  `nombreTipo` varchar(150) DEFAULT NULL,
-  PRIMARY KEY (`idTipoTarea`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- Volcando datos para la tabla admin_casakiki.tareas_tipos: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `tareas_tipos` DISABLE KEYS */;
-INSERT INTO `tareas_tipos` (`idTipoTarea`, `nombreTipo`) VALUES
-	(1, 'control');
-INSERT INTO `tareas_tipos` (`idTipoTarea`, `nombreTipo`) VALUES
-	(2, 'elaboración');
-/*!40000 ALTER TABLE `tareas_tipos` ENABLE KEYS */;
-
--- Volcando estructura para tabla admin_casakiki.usuarios
+-- Volcando estructura para tabla admin_listo.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
   `idUsuario` int(10) NOT NULL AUTO_INCREMENT,
   `nombreUsuario` varchar(200) DEFAULT NULL,
   `emailUsuario` varchar(200) DEFAULT NULL,
-  `password` varchar(25) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
   `idRolUsuario` int(11) DEFAULT NULL,
+  `ciudad` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`idUsuario`) USING BTREE,
   KEY `idRolUsuario` (`idRolUsuario`),
   CONSTRAINT `FK_usuarios_roles` FOREIGN KEY (`idRolUsuario`) REFERENCES `roles` (`idRol`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla admin_casakiki.usuarios: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`idUsuario`, `nombreUsuario`, `emailUsuario`, `password`, `idRolUsuario`) VALUES
-	(1, 'test', 'test@data.es', 'test', 1);
-INSERT INTO `usuarios` (`idUsuario`, `nombreUsuario`, `emailUsuario`, `password`, `idRolUsuario`) VALUES
-	(2, 'admin', 'admin@casakiki.es', 'admin', 1);
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+-- La exportación de datos fue deseleccionada.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
